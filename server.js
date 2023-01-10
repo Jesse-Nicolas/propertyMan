30a0c0d08c5545e03b380a455fa173e548b09f7d
Jesse Nicolas <jn@nevada.unr.edu>
2023-01-10 12:19:37 -0800

    template double-take

--- server.js
+++ server.js
@@ -1,88 +1,30 @@
-// import npm packages
 import 'dotenv/config.js'
 import express from 'express'
-import path from 'path'
-import { fileURLToPath } from 'url'
-import createError from 'http-errors'
-import session from 'express-session'
 import logger from 'morgan'
-import methodOverride from 'method-override'
-import passport from 'passport'
+import cors from 'cors'
+import formData from 'express-form-data'
 
-// import custom middleware
-import { passDataToView } from './middleware/middleware.js'
-
-// connect to MongoDB with mongoose
-import './config/database.js'
-
-// load passport
-import'./config/passport.js'
-
-// import routes
-import { router as indexRouter } from './routes/index.js'
+import { router as profilesRouter } from './routes/profiles.js'
 import { router as authRouter } from './routes/auth.js'
 
-// create the express app
+import './config/database.js'
+
 const app = express()
 
-// view engine setup
-app.set(
-  'views',
-  path.join(path.dirname(fileURLToPath(import.meta.url)), 'views')
-)
-app.set('view engine', 'ejs')
-
-// basic middleware
-app.use(methodOverride('_method'))
+app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(
-  express.static(
-    path.join(path.dirname(fileURLToPath(import.meta.url)), 'public')
-  )
-)
+app.use(formData.parse())
 
-// session middleware
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      sameSite: 'lax',
-    },
-  })
-)
+app.use('/api/profiles', profilesRouter)
+app.use('/api/auth', authRouter)
 
-// passport middleware
-app.use(passport.initialize())
-app.use(passport.session())
-
-// custom middleware
-app.use(passDataToView)
-
-// router middleware
-app.use('/', indexRouter)
-app.use('/auth', authRouter)
-
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404))
+  res.status(404).json({ err: 'Not found' })
 })
 
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
-
-  // render the error page
-  res.status(err.status || 500)
-  res.render('error', {
-    title: `🎊 ${err.status || 500} Error`,
-    user: req.user ? req.user : null,
-    googleClientID: process.env.GOOGLE_CLIENT_ID,
-  })
+  res.status(err.status || 500).json({ err: err.message })
 })
 
 export { app }
