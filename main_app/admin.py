fef77183f1257c21a760f09ecd50fd929fea603f
Jesse Nicolas <jn@nevada.unr.edu>
2023-01-12 15:47:25 -0800

    register models to admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Message, Manager, Client, Task, Action
 
 # Register your models here.
+
+class MessageAdmin(admin.ModelAdmin):
+  list_display = ('author', 'recipient', 'sent', 'text', 'image_url')
+
+class ManagerAdmin(admin.ModelAdmin):
+  list_display = ('name', 'phone', 'bio')
+
+class ClientAdmin(admin.ModelAdmin):
+  list_display = ('name', 'address', 'phone', 'p_manager')
+
+class TaskAdmin(admin.ModelAdmin):
+  list_display = ('client', 'details', 'image_url', 'completed')
+
+class ActionAdmin(admin.ModelAdmin):
+  list_display = ('task', 'start_time', 'details', 'image_url', 'time_spent')
+
+
+admin.site.register(Message, MessageAdmin, Manager, ManagerAdmin, Client, ClientAdmin, Task, TaskAdmin, Action, ActionAdmin)
